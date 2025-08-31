454d16460bb4c349439cc947a8930b02a8b95f2a
GRBarros <gabrielr.barros@outlook.com>
2025-08-31 19:51:28 -0300

    Adicionei um print

--- exemplo/main.py
+++ exemplo/main.py
@@ -2,3 +2,5 @@
 print("Exemplo para o repositório!")
 
 print("LOREM: Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, natus odit nulla minima qui assumenda dolor, in, unde aliquam culpa atque iusto ratione veritatis. Qui accusantium suscipit odit recusandae tempore!")
+
+print("LOREM 2: Lorem ipsum dolor sit amet consectetur, adipisicing elit. A aspernatur corporis iusto hic nulla veritatis corrupti? Suscipit odio quos, aliquam maxime impedit blanditiis qui exercitationem quo repellendus, enim explicabo veritatis.")
